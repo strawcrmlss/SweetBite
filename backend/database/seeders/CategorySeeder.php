1d1d055bc966a1ff43211977d0aafedca6848da9
strawcrmlss <strawcarameliess@gmail.com>
2026-08-02 21:11:39 +0700

    refactor: remove unused category seeder

--- backend/database/seeders/CategorySeeder.php
+++ backend/database/seeders/CategorySeeder.php
@@ -8,6 +8,8 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        
+        // Seeder kategori tidak digunakan pada implementasi akhir.
+        // Data kategori diimpor melalui file SQL/database.
+
     }
 }
