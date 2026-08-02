6fabe81e0467ed42841fa08d015440bd2dd3aae4
strawcrmlss <strawcarameliess@gmail.com>
2026-08-02 21:11:13 +0700

    refactor: remove unused category seeder

--- backend/database/seeders/CategorySeeder.php
+++ backend/database/seeders/CategorySeeder.php
@@ -3,33 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        Category::insert([
-            [
-                'name' => 'Cake',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Bread',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Pastry',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Cookies',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
+        
     }
 }
